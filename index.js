d1aeb31e1887608197ded1ca5a02feaf01309dc5
Promise Nwafor <promisenwafor955@gmail.com>
2020-05-10 05:06:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 const questionDisplay = document.querySelector("h3");
 const options = Array.from(document.querySelectorAll(".option"));
 const startButton = document.querySelector("button");
-let randomDigit;
 let api = [];
-let data;
-let randomNo;
-let optionsArray;
-
+let data, randomNo, optionsArray, correctAnswer;
 
 const fetchApi = () => {
     fetch('https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple')
         .then(resp => resp.json())
-        .then(res => {
-            res.results.map(result => {
-                api.push(result);
-                return api
-            })
-        })
+        .then(res => res.results.map(result => api.push(result)))
+        .catch(err => console.log("Whoops something doesn't seem right " + err))
 }
 fetchApi()
 console.log(api)
@@ -28,22 +20,24 @@ const randomArray = () => {
     return data
 }
 
+const shuffleOptions = (arr => arr.sort(() => Math.random() - 0.5))
+
 const displayData = () => {
     const { question, correct_answer, incorrect_answers } = data;
     optionsArray = [correct_answer, ...incorrect_answers];
-    optionsArray.forEach((opt, i) => {
-        return options[i].textContent = optionsArray[i]
-    })
+    correctAnswer = optionsArray[0]
+    questionDisplay.textContent = question
+    shuffleOptions(optionsArray)
+    optionsArray.forEach((opt, i) => (options[i].textContent = optionsArray[i]))
 }
 
 const checkAnswer = () => {
-    options.forEach(option => {
+    options.forEach((option, i) => {
         option.addEventListener('click', function(){
-            if (option.id == data.answer) {
-                console.log('correct');
-            }else{
-                console.log('wrong')
-            }
+            option.textContent === correctAnswer ? 
+            console.log('correct')
+            :
+            console.log('wrong')
         })
     })
 }
